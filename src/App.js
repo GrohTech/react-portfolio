c8aa3fdd540ea29639ea5651cc5c47f403ac891f
Lauren Groh <legroh@uwm.edu>
2022-01-15 18:09:32 -0600

    delete unnecessary files, clean up App.js, start Header component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className = "App">
+      <header classname = "App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Full Stack Web Developer Lauren Groh
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
